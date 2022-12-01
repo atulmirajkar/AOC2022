e62a62cf4e8c51be983aa983dac3aa21038baa32
Atul Mirajkar <amirajka@epic.com>
2022-12-01 12:14:47 -0600

    first commit

---
+++ Utils/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Util
+{
+    public static class FileUtil
+    {
+        public static List<string>  ReadFile(string filePath){
+            List<string> strList = null;
+            try{
+                using (var sr = new StreamReader(filePath)){
+                    while(sr.Peek()>=0){
+                        if(strList == null){
+                            strList = new List<string>();
+                        }
+                        strList.Add(sr.ReadLine());
+                    }
+                }
+            } catch(Exception e){
+                Console.Write(e.ToString());
+            }
+            return strList;
+        }
+    }
+}
