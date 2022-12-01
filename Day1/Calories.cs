e09a93822a54c01718edd78ba050f65c1a41bc32
atulmirajkar <atulmirajkar@gmail.com>
2022-12-01 14:23:22 -0600

    add stopwatch

--- Day1/Calories.cs
+++ Day1/Calories.cs
@@ -1,71 +1,89 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System;
 using Util;
 using System.Collections.Generic;
 
-namespace AOC 
+namespace AOC
 {
     class Calories
     {
         static void Main(string[] args)
         {
+            var watch = new Stopwatch();
+            watch.Start();
             List<string> calList = FileUtil.ReadFile("./data.txt");
-            // foreach(string str in testList){
-            //     Console.WriteLine(str);
-            // }
             //PrintMaxCalories(calList);
-            PrintTopNCalories(calList,3);
+            PrintTopNCalories(calList, 3);
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
         }
 
-        static void PrintMaxCalories(List<string> calList){
-            if(calList == null || calList.Count == 0 ){
+
+        static void PrintMaxCalories(List<string> calList)
+        {
+            if (calList == null || calList.Count == 0)
+            {
                 Console.WriteLine("Data error");
                 return;
             }
             int maxIdx = 1;
             int maxCal = int.MinValue;
 
-            int sumCal =0;
+            int sumCal = 0;
             int idx = 0;
-            foreach(string cal in calList){
-                if(String.IsNullOrEmpty(cal)){
-                    if(sumCal > maxCal){
+            foreach (string cal in calList)
+            {
+                if (String.IsNullOrEmpty(cal))
+                {
+                    if (sumCal > maxCal)
+                    {
                         maxCal = sumCal;
                         maxIdx = idx;
                     }
-                    sumCal =0;        
+                    sumCal = 0;
                     idx++;
                     continue;
                 }
                 int calVal = int.Parse(cal);
                 sumCal += calVal;
             }
-            Console.WriteLine(maxIdx+":"+maxCal);
+            if (sumCal > maxCal)
+            {
+                maxCal = sumCal;
+                maxIdx = idx;
+            }
+            Console.WriteLine(maxIdx + ":" + maxCal);
         }
 
-        static void PrintTopNCalories(List<string> calList, int n){
-            if(calList == null || calList.Count == 0 ){
+        static void PrintTopNCalories(List<string> calList, int n)
+        {
+            if (calList == null || calList.Count == 0)
+            {
                 Console.WriteLine("Data error");
                 return;
             }
 
-            //min heap of size n
+            //min heap of size n - can also use array of size n and keep sorting that
             PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
-            int sumCal =0;
+            int sumCal = 0;
             int idx = 0;
-            foreach(string cal in calList){
-                if(String.IsNullOrEmpty(cal)){
-                    updatePQ(pq,sumCal,n);
-                    sumCal =0;        
+            foreach (string cal in calList)
+            {
+                if (String.IsNullOrEmpty(cal))
+                {
+                    updatePQ(pq, sumCal, n);
+                    sumCal = 0;
                     idx++;
                     continue;
                 }
                 int calVal = int.Parse(cal);
                 sumCal += calVal;
             }
-            updatePQ(pq,sumCal,n);
+            updatePQ(pq, sumCal, n);
 
             int nSum = 0;
-            while(pq.Count>0){
+            while (pq.Count > 0)
+            {
                 int val = pq.Dequeue();
                 Console.WriteLine(val);
                 nSum += val;
@@ -73,14 +91,18 @@ namespace AOC
             Console.WriteLine("total sum:" + nSum);
         }
 
-        static void updatePQ(PriorityQueue<int, int> pq, int sumCal, int n){
-            if(pq.Count == 0){
+        static void updatePQ(PriorityQueue<int, int> pq, int sumCal, int n)
+        {
+            if (pq.Count == 0)
+            {
                 pq.Enqueue(sumCal, sumCal);
                 return;
             }
-            if(sumCal>pq.Peek()){
+            if (sumCal > pq.Peek())
+            {
                 pq.Enqueue(sumCal, sumCal);
-                if(pq.Count>n){
+                if (pq.Count > n)
+                {
                     pq.Dequeue();
                 }
             }
