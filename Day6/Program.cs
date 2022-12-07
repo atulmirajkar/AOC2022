c2b59f7757a5b37ea110a8222c14ef54b4f044d3
atulmirajkar <atulmirajkar@gmail.com>
2022-12-06 22:44:01 -0600

    minor refactor

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -3,28 +3,25 @@
 List<string> inputList = FileUtil.ReadFile("./data.txt");
 string input = inputList[0];
 //sliding window
-
 int left =0;
 int right =0;
 HashSet<char> set = new HashSet<char>();
 bool found = false;
-for(; right<input.Length; right++){    
+for(; right<input.Length; right++){   
+    //while window is invalid - reduce window size 
     while(set.Contains(input[right])){
         set.Remove(input[left]);
         left++;
     }
 
     set.Add(input[right]);
-    //part 1 - 4
     if((right-left+1)>=14){
         found = true;
         break;
     }
 }
-
 if(!found){
     Console.WriteLine("no window found");
 }
-
 Console.WriteLine(right+1);
 
