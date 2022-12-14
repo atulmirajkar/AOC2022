aa4823bfe09fa655ba09b4a0755e59da14b89fb0
atulmirajkar <atulmirajkar@gmail.com>
2022-12-13 20:25:18 -0600

    working day 13 part 1

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -32,11 +32,13 @@ Tuple<Packet, Packet>
 List<string> inputList = FileUtil.ReadFile("./data.txt");
 Console.WriteLine(part1(inputList));
 
-int part1(List<string> inputList) {
+int part1(List<string> inputList)
+{
     int i = 0;
     int count = 0;
     int pairIdx = 1;
-    while (i < inputList.Count) {
+    while (i < inputList.Count)
+    {
         Console.WriteLine();
         string firstStr = inputList[i++];
         Packet? p1 = ParseAndBuildPacket(firstStr);  //dont send the brackets 
@@ -44,10 +46,12 @@ int part1(List<string> inputList) {
         string secondStr = inputList[i++];
         Packet? p2 = ParseAndBuildPacket(secondStr);  //dont send the brackets 
 
-        if (String.Compare(DisplayPacket(p1), firstStr) != 0) {
+        if (String.Compare(DisplayPacket(p1), firstStr) != 0)
+        {
             throw new ArgumentException();
         }
-        if (String.Compare(DisplayPacket(p2), secondStr) != 0) {
+        if (String.Compare(DisplayPacket(p2), secondStr) != 0)
+        {
             throw new ArgumentException();
         }
         Console.WriteLine(firstStr);
@@ -56,10 +60,13 @@ int part1(List<string> inputList) {
         PacketComparer pc = new PacketComparer();
         int c = pc.Compare(p1, p2);
         Console.WriteLine("Compare:" + c);
-        if (c < 0) {
+        if (c < 0)
+        {
             count += pairIdx;
             Console.WriteLine($"IsSmaller - {pairIdx}");
-        } else {
+        }
+        else
+        {
             Console.WriteLine($"Not Smaller- {pairIdx}");
         }
         pairIdx++;
@@ -69,30 +76,39 @@ int part1(List<string> inputList) {
 }
 
 //recursive parser
-Packet? ParseAndBuildPacket(string str) {
+Packet? ParseAndBuildPacket(string str)
+{
     //exit condition
-    if (String.IsNullOrEmpty(str)) {
+    if (String.IsNullOrEmpty(str))
+    {
         return null;
     }
 
     //recurse
     Packet p = new Packet();
     int splitIdx = -1;
-    if (str[0] == '[') {
+    if (str[0] == '[')
+    {
         splitIdx = getMatching(str, 0);
         p.InnerPacket = ParseAndBuildPacket(str.Substring(1, splitIdx - 1));  //dont send the brackets again
         splitIdx++; //points to comma
-    } else {
+    }
+    else
+    {
         splitIdx = str.IndexOf(','); //index of comma
-        if (splitIdx != -1) {
+        if (splitIdx != -1)
+        {
             p.Val = Convert.ToInt32(str.Substring(0, splitIdx));
-        } else {
+        }
+        else
+        {
             //last packet value e.g. 42
             p.Val = Convert.ToInt32(str);
         }
     }
     //split the string for the next iteration
-    if (splitIdx == -1 || splitIdx > str.Length - 1) {
+    if (splitIdx == -1 || splitIdx > str.Length - 1)
+    {
         //we are done 
         return p;
     }
@@ -102,31 +118,43 @@ Packet? ParseAndBuildPacket(string str) {
 
 }
 
-string DisplayPacket(Packet? p) {
+string DisplayPacket(Packet? p)
+{
     StringBuilder sb = new StringBuilder();
     DisplayPacketRec(p, true, ref sb);
     return sb.ToString();
 }
 
-void DisplayPacketRec(Packet? p, bool isFirst, ref StringBuilder sb) {
-    if (p == null) {
+void DisplayPacketRec(Packet? p, bool isFirst, ref StringBuilder sb)
+{
+    if (p == null)
+    {
         return;
     }
     //either inner packet or val will be populated
-    if (p.Val == null) {
-        if (isFirst) {
+    if (p.Val == null)
+    {
+        if (isFirst)
+        {
             sb.Append("[");
 
-        } else {
+        }
+        else
+        {
             sb.Append(",[");
 
         }
         DisplayPacketRec(p.InnerPacket, true, ref sb);
         sb.Append("]");
-    } else {
-        if (isFirst) {
+    }
+    else
+    {
+        if (isFirst)
+        {
             sb.Append(p.Val);
-        } else {
+        }
+        else
+        {
             sb.Append($",{p.Val}");
 
         }
@@ -134,15 +162,21 @@ void DisplayPacketRec(Packet? p, bool isFirst, ref StringBuilder sb) {
     DisplayPacketRec(p.NextPacket, false, ref sb);
 }
 
-int getMatching(string str, int startIdx) {
+int getMatching(string str, int startIdx)
+{
 
     Stack<char> stack = new();
-    for (int i = startIdx; i < str.Length; i++) {
-        if (str[i] == '[') {
+    for (int i = startIdx; i < str.Length; i++)
+    {
+        if (str[i] == '[')
+        {
             stack.Push(str[i]);
-        } else if (str[i] == ']') {
+        }
+        else if (str[i] == ']')
+        {
             stack.Pop();
-            if (stack.Count == 0) {
+            if (stack.Count == 0)
+            {
                 return i;
             }
         }
@@ -151,33 +185,42 @@ int getMatching(string str, int startIdx) {
 }
 
 
-class PacketComparer : IComparer<Packet> {
-    public int Compare(Packet? x, Packet? y) {
+class PacketComparer : IComparer<Packet>
+{
+    public int Compare(Packet? x, Packet? y)
+    {
         //if both have finished
-        if (x == null && y == null) {
+        if (x == null && y == null)
+        {
             return 0;
         }
 
         //if x has finished 
-        if (x == null) {
+        if (x == null)
+        {
             return -1;
         }
 
         //if y has finished
-        if (y == null) {
+        if (y == null)
+        {
             return 1;
         }
 
         //If both values are integers, the lower integer should come first.
         //the inputs are the same integer; continue checking the next part of the input.
-        if (x.InnerPacket == null && y.InnerPacket == null) {
-            if (x.Val == y.Val) {
+        if (x.InnerPacket == null && y.InnerPacket == null)
+        {
+            if (x.Val == y.Val)
+            {
                 return Compare(x.NextPacket, y.NextPacket);
             }
-            if (x.Val == null || x.Val < y.Val) {
+            if (x.Val == null || x.Val < y.Val)
+            {
                 return -1;
             }
-            if (y.Val == null || x.Val > y.Val) {
+            if (y.Val == null || x.Val > y.Val)
+            {
                 return 1;
             }
         }
@@ -188,15 +231,19 @@ class PacketComparer : IComparer<Packet> {
         //the right list runs out of items first, the inputs are not in 
         //the right order. If the lists are the same length and no comparison 
         //makes a decision about the order, continue checking the next part of the input.
-        if (x.InnerPacket != null && y.InnerPacket != null) {
+        if (x.InnerPacket != null && y.InnerPacket != null)
+        {
             //check inner packets and then go to next
-            if (Compare(x.InnerPacket, y.InnerPacket) < 0) {
+            if (Compare(x.InnerPacket, y.InnerPacket) < 0)
+            {
                 return -1;
             }
-            if (Compare(x.InnerPacket, y.InnerPacket) == 0) {
+            if (Compare(x.InnerPacket, y.InnerPacket) == 0)
+            {
                 return Compare(x.NextPacket, y.NextPacket);
             }
-            if (Compare(x.InnerPacket, y.InnerPacket) > 0) {
+            if (Compare(x.InnerPacket, y.InnerPacket) > 0)
+            {
                 return 1;
             }
 
@@ -207,7 +254,8 @@ class PacketComparer : IComparer<Packet> {
         //For example, if comparing [0,0,0] and 2
         //, convert the right value to [2] (a list containing 2); 
         //the result is then found by instead comparing [0,0,0] and [2].
-        if (x.InnerPacket == null && y.InnerPacket != null) {
+        if (x.InnerPacket == null && y.InnerPacket != null)
+        {
             Packet tempPacket = new Packet();
             tempPacket.NextPacket = x.NextPacket;
             tempPacket.InnerPacket = new Packet();
@@ -215,7 +263,8 @@ class PacketComparer : IComparer<Packet> {
             return Compare(tempPacket, y);
         }
 
-        if (x.InnerPacket != null && y.InnerPacket == null) {
+        if (x.InnerPacket != null && y.InnerPacket == null)
+        {
             Packet tempPacket = new Packet();
             tempPacket.NextPacket = y.NextPacket;
             tempPacket.InnerPacket = new Packet();
@@ -223,23 +272,11 @@ class PacketComparer : IComparer<Packet> {
             return Compare(x, tempPacket);
         }
 
-
-        //[] - x.val = null and x.innerList = null
-
-        // if (x.Val == null && x.InnerPacket == null && y.Val == null && y.InnerPacket == null) {
-        //     return 0;
-        // }
-        // if (x.Val == null && x.InnerPacket == null) {
-        //     return -1;
-        // }
-        // if (y.Val == null && y.InnerPacket == null) {
-        //     return 1;
-        // }
-
         throw new ArgumentException();
     }
 }
-class Packet {
+class Packet
+{
     public int? Val { get; set; }
     public Packet? NextPacket { get; set; }
     public Packet? InnerPacket { get; set; }
