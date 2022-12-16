fce6925d5748cef6dbf76bc6c25ab4a20d85d888
atul <atulmirajkar@gmail.com>
2022-12-15 20:41:28 -0600

    working but too slow part2

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -30,7 +30,7 @@ foreach (string input in inputList) {
 
 // Stopwatch sw = new Stopwatch();
 // sw.Start();
-// Console.WriteLine(part1(pairList, 2000000));
+// Console.WriteLine(part1(pairList, 0, 4000000));
 // sw.Stop();
 // Console.WriteLine($"Elapsed ms: {sw.ElapsedMilliseconds}");
 
@@ -60,110 +60,91 @@ foreach pair
     find distance
         
 */
-int part1(List<SBPair> pairList, int yTarget, int maxCo, ref HashSet<(int, int)> set, ref HashSet<(int, int)> beaconSet) {
+int part1(List<SBPair> pairList, int yTarget, int maxCo) {
     //set containing points on the target row
-    // HashSet<(int, int)> set = new();
-    // HashSet<(int, int)> beaconSet = new();
-
-    if (set.Count == 0 && beaconSet.Count == 0) {
-        foreach (var pair in pairList) {
-            beaconSet.Add(pair.bPoint);
-        }
-        foreach (var sbPair in pairList) {
-            process(sbPair, ref set, beaconSet);
-        }
-    }
-
-    for (int i = 0; i <= maxCo; i++) {
-        if (!beaconSet.Contains((i, yTarget)) && !set.Contains((i, yTarget))) {
-            Console.WriteLine($"{i}:{yTarget}\t");
-        }
-    }
-    // var arr = set.ToArray();
-    // Array.Sort(arr);
-    // foreach (var p in arr) {
-    //     if (p.Item1 < 0 || p.Item1 > 20) {
-    //         continue;
-    //     }
-    //     for (int i = 0; i <= 20; i++) {
-    //         if (!set.Contains((i, yTarget))) {
-    //             Console.Write($"{p.Item1}:{p.Item2}\t");
-    //         }
-    //     }
-    // }
-    return set.Count;
-
-}
-int part2(List<SBPair> pairList, int maxCo) {
     HashSet<(int, int)> set = new();
     HashSet<(int, int)> beaconSet = new();
     foreach (var pair in pairList) {
         beaconSet.Add(pair.bPoint);
     }
     foreach (var sbPair in pairList) {
-        Console.WriteLine(sbPair.sPoint + ":" + sbPair.bPoint);
-        process(sbPair, ref set, beaconSet);
+        process(sbPair, yTarget, ref set, beaconSet);
     }
+    var arr = set.ToArray();
+    Array.Sort(arr);
+    var toparr = arr;
+    foreach (var a in toparr) {
+        Console.WriteLine(a);
+    }
+    return set.Count;
 
+}
+int part2(List<SBPair> pairList, int maxCo) {
+    HashSet<(int, int)> beaconSet = new();
+    foreach (var pair in pairList) {
+        beaconSet.Add(pair.bPoint);
+    }
     List<(int, int)> pointList = new();
-    for (int i = 0; i <= maxCo; i++) {
-        Console.WriteLine(i);
+    for (int i = 0; i < maxCo; i++) {
+        Console.WriteLine($"Row:{i}");
+        HashSet<(int, int)> set = new();
+        foreach (var sbPair in pairList) {
+            // Console.WriteLine(sbPair.sPoint + ":" + sbPair.bPoint);
+
+            process(sbPair, i, ref set, beaconSet);
+        }
         for (int j = 0; j <= maxCo; j++) {
-            if (!beaconSet.Contains((i, j)) && !set.Contains((i, j))) {
+            if (!beaconSet.Contains((j, i)) && !set.Contains((j, i))) {
+                Console.WriteLine((j, i));
                 pointList.Add((i, j));
             }
         }
     }
+
     Console.WriteLine($"{pointList[0].Item1}:{pointList[0].Item2}\t");
     Console.WriteLine(pointList.Count);
     return -1;
 }
 
-void process(SBPair sbPair, ref HashSet<(int, int)> set, HashSet<(int, int)> beaconSet) {
+void process(SBPair sbPair, int yTarget, ref HashSet<(int, int)> set, HashSet<(int, int)> beaconSet) {
     int mDist = getDist(sbPair.sPoint, sbPair.bPoint);
     //vertical expansion
     var upPoint = addPoint(sbPair.sPoint, (0, mDist), true);
     var lowPoint = addPoint(sbPair.sPoint, (0, mDist), false);
-    Console.WriteLine($"Sensor: {sbPair.sPoint} Dist: {mDist} Up: {upPoint} Low: {lowPoint}");
+    // Console.WriteLine($"Sensor: {sbPair.sPoint} Dist: {mDist} Up: {upPoint} Low: {lowPoint}");
 
     //check if yTarget,sBeacon.X lies on upPoint - lowPoint
     // dist between upPoint, lowPoint = dist between upPoint, targetPoint + dist between targetPoint, lowPoint
-
-    var targetPoint = upPoint;
-    while (targetPoint != addPoint(lowPoint, (0, 1), false)) {
-        Console.WriteLine(targetPoint);
-        int upDist = getDist(upPoint, targetPoint);
-        int lowDist = getDist(targetPoint, lowPoint);
-        if (getDist(upPoint, lowPoint) != (upDist + lowDist)) {
-            return;
-        }
-
-        //cover length is going to be minimum of upDist and lowDist
-        int distFromTarget = upDist < lowDist ? upDist : lowDist;
-        int cont = 1 + ((distFromTarget) * 2);
-
-        //add points on yTarget horizontal line
-        if (!beaconSet.Contains(targetPoint))
-            set.Add(targetPoint);
-
-        for (int i = 1; i <= (cont / 2); i++) {
-            (int, int) newPoint = (targetPoint.Item1 - i, targetPoint.Item2);
-            if (!beaconSet.Contains(newPoint)) {
-                // Console.WriteLine($"Add Point: {newPoint.Item1}: {newPoint.Item2}");
-                set.Add(newPoint);
-            }
-
-            (int, int) anotherPoint = (targetPoint.Item1 + i, targetPoint.Item2);
-            if (!beaconSet.Contains(anotherPoint)) {
-                // Console.WriteLine($"Add Point: {anotherPoint.Item1}: {anotherPoint.Item2}");
-                set.Add(anotherPoint);
-            }
-        }
-        //go down
-        targetPoint = addPoint(targetPoint, (0, 1), false);
+    var targetPoint = (sbPair.sPoint.Item1, yTarget);
+    int upDist = getDist(upPoint, targetPoint);
+    int lowDist = getDist(targetPoint, lowPoint);
+    if (getDist(upPoint, lowPoint) != (upDist + lowDist)) {
+        return;
     }
 
+    // Console.WriteLine($"Target: {targetPoint}");
 
+    //cover length is going to be minimum of upDist and lowDist
+    int distFromTarget = upDist < lowDist ? upDist : lowDist;
+    int cont = 1 + ((distFromTarget) * 2);
+
+    //add points on yTarget horizontal line
+    if (!beaconSet.Contains(targetPoint))
+        set.Add(targetPoint);
+
+    for (int i = 1; i <= (cont / 2); i++) {
+        (int, int) newPoint = (targetPoint.Item1 - i, targetPoint.Item2);
+        if (!beaconSet.Contains(newPoint)) {
+            // Console.WriteLine($"Add Point: {newPoint.Item1}: {newPoint.Item2}");
+            set.Add(newPoint);
+        }
+
+        (int, int) anotherPoint = (targetPoint.Item1 + i, targetPoint.Item2);
+        if (!beaconSet.Contains(anotherPoint)) {
+            // Console.WriteLine($"Add Point: {anotherPoint.Item1}: {anotherPoint.Item2}");
+            set.Add(anotherPoint);
+        }
+    }
 }
 
 (int, int) addPoint((int, int) p1, (int, int) p2, bool isSubtract) {
