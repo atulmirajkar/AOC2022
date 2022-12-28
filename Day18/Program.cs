cb318294015364f972b7e96ed14bcd831cf05f50
atulmirajkar <atulmirajkar@gmail.com>
2022-12-27 22:48:25 -0600

    working

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        List<string> inputList = FileUtil.ReadFile("testdata.txt");
+        List<string> inputList = FileUtil.ReadFile("data.txt");
 
         //we are given 1x1x1 cubes
         //we need to find how many surfaces are open
@@ -21,70 +21,69 @@ internal class Program
             int z = Convert.ToInt32(inputArr[2].Trim());
             ptSet.Add((x, y, z));
         }
+        Console.WriteLine($"Part 1: {part1()}");
+        Console.WriteLine($"Part 1: {part2()}");
 
-        Console.WriteLine(part1());
-        Console.WriteLine($"Part2: {part2()}");
-
-        //kind of brute force
-        //for every point check whether the point is in a pocket. If not get
-        //surface area
         int part2()
         {
             int cnt = 0;
             foreach (var pt in ptSet)
             {
-                if (inPocket(pt))
+                cnt += getSurface2(pt);
+            }
+            return cnt;
+        }
+        
+        int getSurface2((int, int, int) pt)
+        {
+            int[,] dir = { { -1, 0, 0 }, { 1, 0, 0 }, { 0, -1, 0 }, { 0, 1, 0 }, { 0, 0, -1 }, { 0, 0, 1 } };
+            int cnt = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                (int, int, int) newPt = (pt.Item1 + dir[i, 0], pt.Item2 + dir[i, 1], pt.Item3 + dir[i, 2]);
+                //if sticking to other cube - surface not open
+                if (ptSet.Contains(newPt))
                 {
                     continue;
                 }
-                cnt += getSurface(pt);
+
+                //if newPt is enclosed - continue
+                HashSet<(int,int,int)> visited = new();
+                visited.Add(newPt);
+                if(!isPtOutside(newPt, visited)){
+                    continue;
+                }
+
+                //else this pt should be outside
+                cnt++;
             }
             return cnt;
-
         }
 
-        bool inPocket((int, int, int) pt)
-        {
-            //Console.WriteLine($"Cur Pt: {pt.Item1}:{pt.Item2}:{pt.Item3}");
-            HashSet<(int, int, int)> visited = new();
-            return DFS(pt, visited, pt);
-        }
-
-        bool DFS((int, int, int) pt, HashSet<(int, int, int)> visited, (int, int, int) origPt)
+        bool isPtOutside((int, int, int) pt, HashSet<(int, int, int)> visited)
         {
             //exit condition
-            if (pt.Item1 < 0 || pt.Item2 < 0 || pt.Item3 < 0 || pt.Item1 > 25 || pt.Item2 > 25 || pt.Item3 > 25)
+            if (pt.Item1 < 0 || pt.Item1 > 25 || pt.Item2 < 0 || pt.Item2 > 25 || pt.Item3 < 0 || pt.Item3 > 25)
             {
-                return false;
-            }
-
-            if (visited.Contains(pt))
-            {
-                //Console.WriteLine($"Visited Pt: {pt.Item1}:{pt.Item2}:{pt.Item3}");
                 return true;
             }
-
-            if (pt != origPt && ptSet.Contains(pt))
-            {
-                //Console.WriteLine($"Another Pt: {pt.Item1}:{pt.Item2}:{pt.Item3}");
-                return true;
-            }
-
-            //Console.WriteLine($"Neigh Pt: {pt.Item1}:{pt.Item2}:{pt.Item3}");
-
-            visited.Add(pt);
 
             int[,] dir = { { -1, 0, 0 }, { 1, 0, 0 }, { 0, -1, 0 }, { 0, 1, 0 }, { 0, 0, -1 }, { 0, 0, 1 } };
             for (int i = 0; i < 6; i++)
             {
                 (int, int, int) newPt = (pt.Item1 + dir[i, 0], pt.Item2 + dir[i, 1], pt.Item3 + dir[i, 2]);
-                if (!DFS(newPt, visited, origPt))
-                {
-                    return false;
+                if (visited.Contains(newPt) || ptSet.Contains(newPt)){
+                    if(ptSet.Contains(newPt)){
+                    }
+                    continue;
+                }
+
+                visited.Add(newPt);
+                if(isPtOutside(newPt, visited)){
+                    return true;
                 }
             }
-            visited.Remove(pt);
-            return true;
+            return false;
         }
 
         int part1()
